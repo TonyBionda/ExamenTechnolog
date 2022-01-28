d3195a1e374c09c5127adf06c1fc204031e1b0c5
Tony Bionda <tonybionda.univ@gmail.com>
2022-01-28 15:17:25 +0100

    Développement des endpoints API Rest

--- app/api.py
+++ app/api.py
@@ -1,5 +1,8 @@
+from typing import Optional
 from fastapi import FastAPI
 from pymongo import MongoClient
+from Vehicule import Vehicule
+from Immatriculation import Immatriculation
 
 app = FastAPI()
 
@@ -13,3 +16,53 @@ def database():
     client = MongoClient(host="mongodb")
     db = client.registre
     return db
+
+
+@app.get("/vehicules")
+def fetch_vehicules():
+    db = database()
+    results = db.vehicules.find()
+    vehicules = []
+    for result in results:
+        v = Vehicule(
+            result["marque"],
+            result["modele"],
+            result["prix"],
+            result["couleur"],
+            Immatriculation(
+                result["immatriculation"]["numero_immat"],
+                result["immatriculation"]["numero_departement"],
+            ),
+        )
+        vehicules.append(v)
+    return vehicules
+
+
+@app.get("/vehicules/prix")
+def fetch_personne_by_prix(eq: Optional[float] = None, gt: Optional[float] = None, lt: Optional[float] = None):
+    db = database()
+    array = []
+    results = None
+    if eq is not None:
+        results = db.vehicules.find({"prix": eq})
+    elif gt is not None and lt is not None:
+        results = db.vehicules.find({"prix": {"$gt": gt, "$lt": lt}})
+    elif gt is not None:
+        results = db.vehicules.find({"prix": {"$gt": gt}})
+    elif lt is not None:
+        results = db.vehicules.find({"prix": {"$lt": lt}})
+    if results is None:
+        return {"error": "Pas de résultat"}
+    for result in results:
+        v = Vehicule(
+            result["marque"],
+            result["modele"],
+            result["prix"],
+            result["couleur"],
+            Immatriculation(
+                result["immatriculation"]["numero_immat"],
+                result["immatriculation"]["numero_departement"],
+            ),
+        )
+        array.append(v)
+    return array
